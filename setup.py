4b7ed0abed2ef2f297b19be0ef7e172e1157281d
Nathan Samra <n.samra@psenterprise.com>
2022-03-28 14:53:36 +0100

    List dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from do import metadata
 setup(name=metadata.name,
       author=metadata.author,
       version=metadata.__version__,
+      install_requires=['appdirs~=1.4.4'],
       packages=find_packages(),
       entry_points={
           'console_scripts': ['do=do.console.entry_point:enter']
