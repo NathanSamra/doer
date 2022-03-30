384b2940cc2ab9554b1321eaa0bc8f86ae72251f
nathans <nathans@carallon.com>
2022-03-30 19:26:13 +0100

    Use template for today page

--- doer/web/views.py
+++ doer/web/views.py
@@ -1,17 +1,13 @@
 from datetime import date
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from doer import storage, config
 from doer.model import Data
 
 
-def today(_request):
+def today(request):
     data = Data(storage.database(), config.context())
     today_ = data.day(date.today())
-    today_str = ''
-    for i, priority in enumerate(today_.priorities, 1):
-        today_str += f'{i}. {priority}\n'
-
-    return HttpResponse(f"Today's priorities:\n\n{today_str}", content_type='text/plain')
+    context = {'today': today_}
+    return render(request, 'today.html', context)
