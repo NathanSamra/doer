deeec67a526f7108d6b92f551bcc980030aadaef
nathans <nathans@carallon.com>
2022-03-31 10:24:19 +0100

    Move requirements to requirements.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from doer import metadata
 setup(name=metadata.name,
       author=metadata.author,
       version=metadata.__version__,
-      install_requires=['appdirs~=1.4.4'],
       packages=find_packages(),
       entry_points={
           'console_scripts': ['doer=doer.console:enter']
