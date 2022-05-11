b75cb40eafebbb77aa517c107d80cef08ebdf8ea
Nathan Samra <n.samra@psenterprise.com>
2022-05-11 13:50:27 +0100

    Make date_ cmdline argument mandatory

--- doer/console/enter.py
+++ doer/console/enter.py
@@ -6,51 +6,34 @@ from doer import metadata
 from doer.console.client import Client
 
 
-def _tomorrow() -> date:
-    return date.today() + timedelta(days=1)
+def _date_from_arg(arg) -> date:
+    today = date.today()
 
+    aliases = {
+        "today": today,
+        "tomorrow": date.today() + timedelta(days=1),
+    }
 
-def _date_from_args(args) -> date:
-    date_ = _tomorrow()
-    if args.today:
-        date_ = date.today()
-    elif args.day is not None:
-        date_ = date.fromisoformat(args.day)
+    if arg in aliases:
+        return aliases[arg]
 
-    return date_
-
-
-def _add_date_group(parser):
-    group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument('--today', action='store_true', help='Today')
-    group.add_argument('--tomorrow', action='store_true', help='Tomorrow')
-    group.add_argument('--day', type=str, help='Day in ISO format')
+    return date.fromisoformat(arg)
 
 
 def _add_plan_action(parsers):
     def action(args):
         client = Client()
-        client.plan_priorities(_date_from_args(args))
+        client.plan_priorities(_date_from_arg(args.date_))
 
     plan_parser = parsers.add_parser('plan')
-    _add_date_group(plan_parser)
+    plan_parser.add_argument('date_', type=str, help='Date to plan')
     plan_parser.set_defaults(func=action)
-
-
-def _date_from_str(date_str) -> date:
-    if date_str == 'today':
-        return date.today()
-
-    if date_str == 'tomorrow':
-        return _tomorrow()
-
-    return date.fromisoformat(date_str)
 
 
 def _add_copy_action(parsers):
     def action(args):
         client = Client()
-        client.copy_priorities(_date_from_str(args.from_), _date_from_str(args.to))
+        client.copy_priorities(_date_from_arg(args.from_), _date_from_arg(args.to))
 
     copy_parser = parsers.add_parser('copy')
     copy_parser.add_argument('from_', type=str, help='Date to copy from. Can be \'today\', \'tomorrow\', or a date in '
@@ -63,10 +46,10 @@ def _add_copy_action(parsers):
 def _add_show_action(parsers):
     def action(args):
         client = Client()
-        client.show(_date_from_args(args))
+        client.show(_date_from_arg(args.date_))
 
     show_parser = parsers.add_parser('show')
-    _add_date_group(show_parser)
+    show_parser.add_argument('date_', type=str, help='Date to show')
     show_parser.set_defaults(func=action)
 
 
@@ -133,7 +116,7 @@ def _add_set_focus(parsers):
 
 
 def _add_start_break(parsers):
-    def action(args):
+    def action():
         Client().start_break()
 
     start_break_parser = parsers.add_parser('start_break')
@@ -141,7 +124,7 @@ def _add_start_break(parsers):
 
 
 def _add_end_break(parsers):
-    def action(args):
+    def action():
         Client().end_break()
 
     end_break_parser = parsers.add_parser('end_break')
@@ -149,7 +132,7 @@ def _add_end_break(parsers):
 
 
 def _add_end_day(parsers):
-    def action(args):
+    def action():
         Client().end_day()
 
     end_day_parser = parsers.add_parser('end_day')
