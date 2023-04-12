1612c6cf7a1d71b0679c1ec6a3e1dfb966262a28
Nathan Samra <n.samra@siemens.com>
2023-04-12 17:41:48 +0100

    Add list of commands

--- src/cli.rs
+++ src/cli.rs
@@ -1,5 +1,30 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about)]
-pub struct Cli {}
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    /// Plan priorities
+    Plan,
+    /// Copy priorities
+    Copy,
+    /// Show priorities
+    Show,
+    /// Tick priorities
+    Tick,
+    /// Work context
+    Context,
+    /// Current focus
+    Focus,
+    /// Break from work
+    Break,
+    /// End the day
+    EndDay,
+    /// Add a note to the day
+    Note,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 mod cli;
 
-use crate::cli::Cli;
+use crate::cli::{Cli, Command};
 
 use clap::Parser;
 
 fn main() {
-    Cli::parse();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Command::Plan => {}
+        Command::Copy => {}
+        Command::Show => {}
+        Command::Tick => {}
+        Command::Context => {}
+        Command::Focus => {}
+        Command::Break => {}
+        Command::EndDay => {}
+        Command::Note => {}
+    }
 }
