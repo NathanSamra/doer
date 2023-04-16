eb2ae25aad9f66fa257e6b9dca1ee1f8d0777fe0
Nathan Samra <me@nathansamra.com>
2023-04-16 18:09:05 +0100

    Add version to database file

--- src/database/data.rs
+++ src/database/data.rs
@@ -1,4 +1,5 @@
 use crate::database::context::Context;
+use semver::Version;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
@@ -6,6 +7,7 @@ use std::collections::HashMap;
 pub struct Data {
     pub context: String,
     pub contexts: HashMap<String, Context>,
+    version: Version,
 }
 
 impl Default for Data {
@@ -13,6 +15,7 @@ impl Default for Data {
         Self {
             context: "default".to_string(),
             contexts: HashMap::from([("default".to_string(), Context::default())]),
+            version: Version::parse(env!("CARGO_PKG_VERSION")).unwrap(),
         }
     }
 }
