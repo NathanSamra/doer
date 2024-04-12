4b3458b1d0430b4518e05b2621b6e65e564e7835
Nathan Samra <n.samra@siemens.com>
2024-04-12 18:14:36 +0100

    Add command arguments

--- src/console/enter.rs
+++ src/console/enter.rs
@@ -1,29 +1,59 @@
 use clap::{Parser, Subcommand};
 
+type PriorityId = i32;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Command>,
+    command: Command,
 }
 
+// TODO: Add descriptions for the commands.
 // TODO: Rethink all of these. Maybe come up with a resource based approach instead of actions
 #[derive(Subcommand, Debug)]
 enum Command {
-    Plan {},
-    Copy {},
-    Show {},
+    Plan {
+        /// Date to plan
+        date: String,
+    },
+    Copy {
+        /// Date/day to copy from
+        from: String,
+        /// Date/day to copy to
+        to: String,
+    },
+    Show {
+        /// Date to show
+        date: String,
+    },
     ShowLast {},
-    Tick {},
-    UnTick {},
+    Tick {
+        // TODO: Add description
+        id: PriorityId,
+    },
+    UnTick {
+        // TODO: Add description
+        id: PriorityId,
+    },
     Context {},
     Contexts {},
-    SetContext {},
-    SetFocus {},
+    SetContext {
+        /// Context to switch to
+        context: String,
+    },
+    SetFocus {
+        // TODO: Consider being more explicit over ID or name, i.e two different variables here.
+        /// name or ID of focus
+        focus: String,
+    },
     StartBreak {},
     EndBreak {},
     EndDay {},
-    Note {},
+    Note {
+        /// Note for today
+        note: String,
+    },
 }
 
 pub fn enter() {
