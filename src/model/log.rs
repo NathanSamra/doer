f3677baf7f74a79d2c043dc0315e1b05540eec89
Nathan Samra <n.samra@siemens.com>
2024-04-21 16:48:45 +0100

    Add Log tests

--- src/model/log.rs
+++ src/model/log.rs
@@ -30,23 +30,6 @@ impl Log {
         Ok(())
     }
 
-    pub fn end_day(&mut self) -> Result<(), LogError> {
-        match self.entries.last() {
-            None => Err(LogError::DayNeverStarted),
-            Some(entry) => match entry.type_ {
-                EntryType::Focus { .. } => {
-                    self.entries.push(Entry::now(EntryType::DayEnded));
-                    Ok(())
-                }
-                EntryType::Unfocused => {
-                    self.entries.push(Entry::now(EntryType::DayEnded));
-                    Ok(())
-                }
-                EntryType::DayEnded => Ok(()),
-            },
-        }
-    }
-
     pub fn start_unfocused(&mut self) -> Result<(), LogError> {
         match self.entries.last() {
             None => {
@@ -60,9 +43,18 @@ impl Log {
                 }
                 EntryType::Unfocused => Err(LogError::AlreadyUnfocused),
                 EntryType::DayEnded => {
-                    self.entries.pop();
-                    self.entries.push(Entry::now(EntryType::Unfocused));
-                    Ok(())
+                    let day_ended = self.entries.pop().unwrap();
+                    let last_entry = self.entries.last().unwrap();
+                    match last_entry.type_ {
+                        EntryType::Focus(_) => {
+                            self.entries.push(day_ended.to_unfocused());
+                            Ok(())
+                        }
+                        EntryType::Unfocused => Ok(()),
+                        EntryType::DayEnded => {
+                            panic!("Day ended twice");
+                        }
+                    }
                 }
             },
         }
@@ -88,10 +80,38 @@ impl Log {
                     Ok(())
                 }
                 EntryType::DayEnded => {
-                    self.entries.pop();
+                    let day_ended = self.entries.pop().unwrap();
+                    let last_entry = self.entries.last().unwrap();
+                    match last_entry.type_ {
+                        EntryType::Focus(_) => {
+                            self.entries.push(day_ended.to_unfocused());
+                        }
+                        EntryType::Unfocused => {}
+                        EntryType::DayEnded => {
+                            panic!("Day ended twice");
+                        }
+                    };
+
                     self.entries.push(Entry::now(EntryType::Focus(task_id)));
                     Ok(())
                 }
+            },
+        }
+    }
+
+    pub fn end_day(&mut self) -> Result<(), LogError> {
+        match self.entries.last() {
+            None => Err(LogError::DayNeverStarted),
+            Some(entry) => match entry.type_ {
+                EntryType::Focus { .. } => {
+                    self.entries.push(Entry::now(EntryType::DayEnded));
+                    Ok(())
+                }
+                EntryType::Unfocused => {
+                    self.entries.push(Entry::now(EntryType::DayEnded));
+                    Ok(())
+                }
+                EntryType::DayEnded => Err(LogError::DayAlreadyEnded),
             },
         }
     }
@@ -110,9 +130,16 @@ impl Entry {
             start: Local::now().naive_local(),
         }
     }
+
+    pub fn to_unfocused(&self) -> Self {
+        Self {
+            type_: EntryType::Unfocused,
+            start: self.start,
+        }
+    }
 }
 
-#[derive(Clone, Deserialize, Serialize)]
+#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 pub enum EntryType {
     Focus(TaskId),
     Unfocused,
@@ -131,4 +158,296 @@ pub enum LogError {
     DayNeverStarted,
     #[error("Already focused on that focus")]
     AlreadyFocusedOnFocus,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_has_no_entries() {
+        let log = Log::default();
+        assert!(log.entries().is_empty());
+    }
+
+    #[test]
+    fn new_is_empty() {
+        let log = Log::default();
+        assert!(log.is_empty());
+    }
+
+    #[test]
+    fn start_day_is_not_empty() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        assert!(!log.is_empty());
+    }
+
+    #[test]
+    fn start_day_is_unfocused() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused]
+        );
+    }
+
+    #[test]
+    fn start_day_twice_fails() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        assert!(log.start_day().is_err());
+    }
+
+    #[test]
+    fn start_unfocused_after_start_day_fails() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        assert!(log.start_unfocused().is_err());
+    }
+
+    #[test]
+    fn start_unfocused_without_start_day() {
+        let mut log = Log::default();
+        log.start_unfocused().unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused]
+        );
+    }
+
+    #[test]
+    fn start_two_unfocused_fails() {
+        let mut log = Log::default();
+        log.start_unfocused().unwrap();
+        assert!(log.start_unfocused().is_err());
+    }
+
+    #[test]
+    fn start_focus_after_start_day() {
+        let mut log = Log::default();
+        let task_id = TaskId::default();
+        log.start_day().unwrap();
+        log.start_focus(task_id).unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused, EntryType::Focus(task_id)]
+        );
+    }
+
+    #[test]
+    fn start_focus_without_start_day() {
+        let mut log = Log::default();
+        let task_id = TaskId::default();
+        log.start_focus(task_id).unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Focus(task_id)]
+        );
+    }
+
+    #[test]
+    fn start_focus_after_start_unfocused() {
+        let mut log = Log::default();
+        let task_id = TaskId::default();
+        log.start_unfocused().unwrap();
+        log.start_focus(task_id).unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused, EntryType::Focus(task_id)]
+        );
+    }
+
+    #[test]
+    fn start_the_same_focus_twice_fails() {
+        let mut log = Log::default();
+        let task_id = TaskId::default();
+        log.start_focus(task_id).unwrap();
+        assert!(log.start_focus(task_id).is_err());
+    }
+
+    #[test]
+    fn start_two_different_focuses() {
+        let mut log = Log::default();
+        let task_id1 = TaskId::now_v7();
+        let task_id2 = TaskId::now_v7();
+        log.start_focus(task_id1).unwrap();
+        log.start_focus(task_id2).unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Focus(task_id1), EntryType::Focus(task_id2)]
+        );
+    }
+
+    #[test]
+    fn end_day_after_start_day() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        log.end_day().unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused, EntryType::DayEnded]
+        );
+    }
+
+    #[test]
+    fn end_day_without_start_day_fails() {
+        let mut log = Log::default();
+        assert!(log.end_day().is_err());
+    }
+
+    #[test]
+    fn end_day_after_start_unfocused() {
+        let mut log = Log::default();
+        log.start_unfocused().unwrap();
+        log.end_day().unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused, EntryType::DayEnded]
+        );
+    }
+
+    #[test]
+    fn end_day_after_start_focus() {
+        let mut log = Log::default();
+        let task_id = TaskId::default();
+        log.start_focus(task_id).unwrap();
+        log.end_day().unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Focus(task_id), EntryType::DayEnded]
+        );
+    }
+
+    #[test]
+    fn end_day_twice_fails() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        log.end_day().unwrap();
+        assert!(log.end_day().is_err());
+    }
+
+    #[test]
+    fn start_day_after_end_day_fails() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        log.end_day().unwrap();
+        assert!(log.start_day().is_err());
+    }
+
+    #[test]
+    fn unfocused_after_end_day() {
+        let mut log = Log::default();
+        log.start_day().unwrap();
+        log.end_day().unwrap();
+        log.start_unfocused().unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused]
+        );
+    }
+
+    #[test]
+    fn unfocused_after_focus_then_end_day() {
+        let mut log = Log::default();
+        let task_id = TaskId::default();
+        log.start_focus(task_id).unwrap();
+        log.end_day().unwrap();
+        log.start_unfocused().unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Focus(task_id), EntryType::Unfocused]
+        );
+    }
+
+    #[test]
+    fn focus_after_end_day() {
+        let mut log = Log::default();
+        let task_id = TaskId::default();
+        log.start_day().unwrap();
+        log.end_day().unwrap();
+        log.start_focus(task_id).unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[EntryType::Unfocused, EntryType::Focus(task_id)]
+        );
+    }
+
+    #[test]
+    fn different_focus_after_end_day() {
+        let mut log = Log::default();
+        let task_id1 = TaskId::now_v7();
+        let task_id2 = TaskId::now_v7();
+        log.start_focus(task_id1).unwrap();
+        log.end_day().unwrap();
+        log.start_focus(task_id2).unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[
+                EntryType::Focus(task_id1),
+                EntryType::Unfocused,
+                EntryType::Focus(task_id2)
+            ]
+        );
+    }
+
+    #[test]
+    fn the_same_focus_after_end_day() {
+        let mut log = Log::default();
+        let task_id = TaskId::now_v7();
+        log.start_focus(task_id).unwrap();
+        log.end_day().unwrap();
+        log.start_focus(task_id).unwrap();
+        assert_eq!(
+            log.entries()
+                .iter()
+                .map(|entry| entry.type_.clone())
+                .collect::<Vec<EntryType>>(),
+            &[
+                EntryType::Focus(task_id),
+                EntryType::Unfocused,
+                EntryType::Focus(task_id)
+            ]
+        );
+    }
 }
